7fb99b9e0137813b00090d7a38a3ccd48f8b4f3b
max4-dev <maksim.bogomyakov@gmail.com>
2021-11-26 19:31:42 +0300

    added catalog pages

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,33 @@
 $(function () {
+
+  $(".filter-price__input").ionRangeSlider({
+    type: "double",
+    onStart: function (data) {
+      $('.filter-price__from').text(data.from),
+        $('.filter-price__to').text(data.to)
+    },
+    onChange: function (data) {
+      $('.filter-price__from').text(data.from),
+      $('.filter-price__to').text(data.to)
+    }
+  });
+
+  $(".star").rateYo({
+    ratedFill: "#ffcc00",
+    normalFill: "#d6d6d6",
+    readOnly: true,
+    starWidth: "11px",
+    spacing: "7px"
+  });
+
+  $('.products__star').rateYo({
+    ratedFill: "#ffcc00",
+    normalFill: "#d6d6d6",
+    readOnly: true,
+    starWidth: "20px",
+    spacing: "13px"
+  });
+  
   $('.top-slider__inner').slick({
     dots: true,
     arrows: false,
@@ -18,7 +47,7 @@ $(function () {
   var mixer1 = mixitup(containerEl1, config);
   var mixer2 = mixitup(containerEl2, config);
 
-  $('.products__btn-favorite').on('click', function(){
-    $(this).toggleClass('products__btn--active');
-  });
+  // $('.products__btn-favorite').on('click', function(){
+  //   $(this).toggleClass('products__btn--active');
+  // });
 });
